fc41ac7377c2043271251cfbf1611a8dfad1e79d
fjh <1731970489@qq.com>
2023-02-02 14:32:53 +0800

    spring security 目录调整

---
+++ spring-security/spring-security-starter/src/main/java/com/fjh/security/SpringSecurityStarterApplication.java
@@ -0,0 +1,13 @@
+package com.fjh.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityStarterApplication.class, args);
+    }
+
+}
